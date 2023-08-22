a68ae2b8035587a6b8078a6d2985dc1e9fc3c03d
Lucas Caraballo <lucascaraballo15@gmail.com>
2023-08-22 00:28:43 -0300

    responsive finish

--- index.php
+++ index.php
@@ -9,41 +9,59 @@
         
         <h2 class="entradas__title">Ultimas publicaciones <i class="fa-solid fa-pen-to-square entradas__icon"></i></h2>
         
-        <a href="www.google.com" target="_blank" class="entradas__entrada">
+        <div class="main__entradas-container">
+            <a href="www.google.com" target="_blank" class="entradas__entrada">
 
-            <div class="entrada__img-container">
-                <img src="./assets/img/post-01.webp" alt="Post Picture" class="entrada__img">
-            </div>
-            
-            <div class="entrada__container">
-
-                <div class="entrada__content">
-                    <h2 class='entrada__title'>Con mamá</h2>
-                    <h3 class='entrada__author'>Camila Vene</h3>
-                    <p class='entrada__fecha'>2023-08-01</p>    
-                    <p class="entrada__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos sint consequatur unde ea.</p>
+                <div class="entrada__img-container">
+                    <img src="./assets/img/post-02.webp" alt="Post Picture" class="entrada__img">
                 </div>
-            </div>
 
-        </a>
+                <div class="entrada__container">
 
-        <a href="www.google.com" target="_blank" class="entradas__entrada">
-
-            <div class="entrada__img-container">
-                <img src="./assets/img/post-01.webp" alt="Post Picture" class="entrada__img">
-            </div>
-            
-            <div class="entrada__container">
-
-                <div class="entrada__content">
-                    <h2 class='entrada__title'>Con mamá</h2>
-                    <h3 class='entrada__author'>Camila Vene</h3>
-                    <p class='entrada__fecha'>2023-08-01</p>    
-                    <p class="entrada__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos sint consequatur unde ea.</p>
+                    <div class="entrada__content">
+                        <h2 class='entrada__title'>Que lindo es chapar</h2>
+                        <h3 class='entrada__author'>Camila Vene</h3>
+                        <p class='entrada__fecha'>2023-08-01</p>
+                    </div>
                 </div>
-            </div>
 
-        </a>
+            </a>
+
+            <a href="www.google.com" target="_blank" class="entradas__entrada">
+
+                <div class="entrada__img-container">
+                    <img src="./assets/img/post-01.webp" alt="Post Picture" class="entrada__img">
+                </div>
+
+                <div class="entrada__container">
+
+                    <div class="entrada__content">
+                        <h2 class='entrada__title'>Con mamá</h2>
+                        <h3 class='entrada__author'>Camila Vene</h3>
+                        <p class='entrada__fecha'>2023-08-01</p>    
+                    </div>
+                </div>
+
+            </a>
+
+            <a href="www.google.com" target="_blank" class="entradas__entrada">
+
+                <div class="entrada__img-container">
+                    <img src="./assets/img/post-01.webp" alt="Post Picture" class="entrada__img">
+                </div>
+
+                <div class="entrada__container">
+
+                    <div class="entrada__content">
+                        <h2 class='entrada__title'>Con mamá</h2>
+                        <h3 class='entrada__author'>Camila Vene</h3>
+                        <p class='entrada__fecha'>2023-08-01</p>    
+                    </div>
+                </div>
+
+            </a>
+
+        </div>
 
     </section>
 
